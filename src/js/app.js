b11e6e0119caa6c8e72b0ed7d280cbddebf05044
manegame <manusnijhoff@gmail.com>
2017-01-12 03:17:56 +0100

    Wednesday commit

--- src/js/app.js
+++ src/js/app.js
@@ -3,9 +3,42 @@
     "use strict";
 
     global.$    = require("jquery");
+  	window.jQuery = $;
+
+	function isElementInViewport (el) {
+
+	      //special bonus for those using jQuery
+	      if (typeof jQuery === "function" && el instanceof jQuery) {
+	          el = el[0];
+	      }
+
+	      var rect = el.getBoundingClientRect();
+
+	      return (
+	          rect.top >= 0 &&
+	          rect.left >= 0 &&
+	          rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+	          rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
+	      );
+	  }
 
     $(function () {
 
+
+    $('#index').on('click', function(){
+      $('html, body').animate({
+        scrollTop: $(window).offset().top
+      }, 800);
+    });
+
+	$(window).on('scroll', function(){
+	    $('.block').each(function(){
+	    	var isVisible = isElementInViewport(this);
+	    	if(isVisible){
+	    		console.log(isVisible);
+	    	}
+	    });
+	});
 
     });
 
